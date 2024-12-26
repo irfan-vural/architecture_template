af03a2c7ea30cc7199603e2e0bf48f704994ab11
irfan vural <irfanvural.dev@gmail.com>
2024-12-26 13:37:11 +0300

    implemented first version

---
+++ android/app/src/main/kotlin/com/irfan/architecture/architecture_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.irfan.architecture.architecture_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/feature/home/view/widget/home_appbar.dart
@@ -0,0 +1,13 @@
+part of "../home_view.dart";
+
+class _HomeAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
